368927effe5356b545a43889c29fca83e73647a3
williankargo <pinkuanxie@gmail.com>
2022-10-19 21:59:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js: start script
+var server = require("./server"); // 請求檔案使用剛剛匯出的模組
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
